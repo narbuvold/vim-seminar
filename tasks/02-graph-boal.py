da5dc5107a5e3102d092bd9c697228f96bb52e3d
henning <henna497@student.liu.se>
2015-09-22 09:11:58 +0200

    added task

---
+++ tasks/02-graph-boal.py
@@ -0,0 +1,13 @@
+graph = {
+        'Ada': ['Alan', 'Herbert', 'Kurt'],
+        'Alan': ['John'],
+        'Bill': [],
+        'Charles': ['Bill', 'Steve'],
+        'Edsger': [],
+        'Grace': ['Linus'],
+        'Herbert': ['Charles', 'Grace'],
+        'John': [],
+        'Kurt': [],
+        'Linus': ['Edsger'],
+        'Steve': []
+        }
---
+++ tasks/02-graph-start.py
@@ -0,0 +1,13 @@
+graph = {
+        'Ada': ['Alan', 'Herbert', 'Kurt'],
+        'Alan': ['John'],
+        'Herbert': ['Charles', 'Grace'],
+        'Kurt': [],
+        'John': [],
+        'Charles': ['Bill', 'Steve'],
+        'Grace': ['Linus'],
+        'Bill': [],
+        'Steve': []
+        'Linus': ['Edsger'],
+        'Edsger': [],
+        }
